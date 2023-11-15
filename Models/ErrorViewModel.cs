084c9c87c92d2a2fe5c32db70749339b21695c61
denzel-sierra <denzel.sierra@ujcv.edu.hn>
2023-11-15 10:35:29 -0600

    Commit inicial con el proyecto en blanco, incluyendo librerías a usar.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto3erParcialGrupo3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
