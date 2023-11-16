c1dfaefff5a1a1f8d13c365df8029cc955624c13
merarissanchez <150539651+merarissanchez@users.noreply.github.com>
2023-11-15 21:50:50 -0600

    Creación de la Entidad DetalleServicioFactura

---
+++ Models/DetalleServicioFactura.cs
@@ -0,0 +1,12 @@
+﻿namespace Proyecto3erParcialGrupo3.Models
+{
+    public class DetalleServicioFactura
+    {
+        public int IDDetalleFactura { get; set; }
+        public int IDFactura { get; set; }
+        public int IDServicio { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public decimal SubTotalLinea { get; set; }
+    }
+}
