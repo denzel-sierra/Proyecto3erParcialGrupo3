d3bec5afe5476c28324628348fa939233bd7e393
merarissanchez <150539651+merarissanchez@users.noreply.github.com>
2023-11-15 21:52:32 -0600

    Creación de la Entidad EncabezadoFactura

---
+++ Models/EncabezadoFactura.cs
@@ -0,0 +1,17 @@
+﻿namespace Proyecto3erParcialGrupo3.Models
+{
+    public class EncabezadoFactura
+    {
+        public int IDFactura { get; set; }
+        public int IDCorrelativoSAR { get; set; }
+        public int IDEmpleado { get; set; }
+        public int IDUsuario { get; set; }
+        public string NumeroFacturaSAR { get; set; }
+        public DateTime FechaFactura { get; set; }
+        public decimal SubTotalFactura { get; set; }
+        public decimal DescuentoFactura { get; set; }
+        public decimal ImpuestoFactura { get; set; }
+        public decimal TotalFactura { get; set; }
+        public bool Eliminada { get; set; }
+    }
+}
