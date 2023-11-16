b48f2e88ee8e970774174f7e1e7f7c0ecfeebec8
merarissanchez <150539651+merarissanchez@users.noreply.github.com>
2023-11-15 21:54:42 -0600

    Creación de la Entidad Habitaciones

---
+++ Models/Habitaciones.cs
@@ -0,0 +1,11 @@
+﻿namespace Proyecto3erParcialGrupo3.Models
+{
+    public class Habitaciones
+    {
+        public int IDHabitacion { get; set; }
+        public string TipoHabitacion { get; set; }
+        public decimal Tarifa { get; set; }
+        public bool Disponibilidad { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
