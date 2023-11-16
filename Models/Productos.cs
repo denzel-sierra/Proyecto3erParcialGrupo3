42753ccd1f615a79b32b27bd49fc0f9fcf885f3f
merarissanchez <150539651+merarissanchez@users.noreply.github.com>
2023-11-15 22:00:50 -0600

    Creación de la Entidad Productos

---
+++ Models/Productos.cs
@@ -0,0 +1,11 @@
+﻿namespace Proyecto3erParcialGrupo3.Models
+{
+    public class Productos
+    {
+        public int IDProducto { get; set; }
+        public string NombreProducto { get; set; }
+        public string Descripcion { get; set; }
+        public decimal PrecioUnitario { get; set; }
+        public int Existencias { get; set; }
+    }
+}
