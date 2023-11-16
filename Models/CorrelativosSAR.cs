e40b7281f92e842258c2012cdcc74ca9e729dacc
merarissanchez <150539651+merarissanchez@users.noreply.github.com>
2023-11-15 21:45:45 -0600

    Creación de la Entidad CorrelativosSAR

---
+++ Models/CorrelativosSAR.cs
@@ -0,0 +1,15 @@
+﻿namespace Proyecto3erParcialGrupo3.Models
+{
+    public class CorrelativosSAR
+    {
+        public int IDCorrelativoSAR { get; set; }
+        public string NumeroCAI { get; set; }
+        public int NumeroInicial { get; set; }
+        public int NumeroFinal { get; set; }
+        public int UltimoUtilizado { get; set; }
+        public DateTime FechaInicial { get; set; }
+        public DateTime FechaLimite { get; set; }
+        public DateTime FechaFinal { get; set; }
+        public bool Finalizado { get; set; }
+    }
+}
