622c0ba7d931f43cb1a07771cc4b7a4d76f9b828
merarissanchez <150539651+merarissanchez@users.noreply.github.com>
2023-11-19 15:30:01 -0600

    Correción de la Entidad Productos

--- Models/Productos.cs
+++ Models/Productos.cs
@@ -2,7 +2,7 @@
 {
     public class Productos
     {
-        public int IDProducto { get; set; }
+        public Guid IDProducto { get; set; }
         public string NombreProducto { get; set; }
         public string Descripcion { get; set; }
         public decimal PrecioUnitario { get; set; }
