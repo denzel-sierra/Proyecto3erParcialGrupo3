a451afc46db3e7e943d9b0a461d385da23ce084d
merarissanchez <150539651+merarissanchez@users.noreply.github.com>
2023-11-19 15:30:58 -0600

    Correción de la Entidad Reserva

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -5,7 +5,7 @@
         public int IDReserva { get; set; }
         public int IDUsuario { get; set; }
         public int IDHabitacion { get; set; }
-        public int IDFactura { get; set; }
+        public Guid IDFactura { get; set; }
         public DateTime FechaCheckin { get; set; }
         public DateTime FechaCheckOut { get; set; }
         public string EstadoReserva { get; set; }
