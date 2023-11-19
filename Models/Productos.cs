1508ae021a2fc782b7a7c1803f87656c2e945223
merarissanchez <150539651+merarissanchez@users.noreply.github.com>
2023-11-19 14:07:37 -0600

    Relaciones de la Entidad Productos

--- Models/Productos.cs
+++ Models/Productos.cs
@@ -7,5 +7,8 @@
         public string Descripcion { get; set; }
         public decimal PrecioUnitario { get; set; }
         public int Existencias { get; set; }
+
+        // Relaciones
+        public ICollection<DetalleProductoFactura> DetallesFacturas { get; set; }
     }
 }
