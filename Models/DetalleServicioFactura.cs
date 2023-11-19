85c2223ebf221a6e92e7f410293dec92bb3d5c74
merarissanchez <150539651+merarissanchez@users.noreply.github.com>
2023-11-19 15:26:39 -0600

    Corrección de la Entidad DetalleServicioFactura

--- Models/DetalleServicioFactura.cs
+++ Models/DetalleServicioFactura.cs
@@ -2,9 +2,9 @@
 {
     public class DetalleServicioFactura
     {
-        public int IDDetalleFactura { get; set; }
-        public int IDFactura { get; set; }
-        public int IDServicio { get; set; }
+        public Guid IDDetalleFactura { get; set; }
+        public Guid IDFactura { get; set; }
+        public Guid IDServicio { get; set; }
         public int Cantidad { get; set; }
         public decimal PrecioUnitario { get; set; }
         public decimal SubTotalLinea { get; set; }
