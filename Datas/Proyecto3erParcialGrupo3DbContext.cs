e97581bd619618f8c223247ed8f8568991e13e65
denzel-sierra <denzel.sierra@ujcv.edu.hn>
2023-11-19 22:40:33 -0600

    Plantilla para el dbcontext

--- Datas/Proyecto3erParcialGrupo3DbContext.cs
+++ Datas/Proyecto3erParcialGrupo3DbContext.cs
@@ -1,76 +1,33 @@
-﻿using Proyecto3erParcialGrupo3.Models;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
+﻿using Microsoft.EntityFrameworkCore;
 using Proyecto3erParcialGrupo3.Models;
+using static Proyecto3erParcialGrupo3.Data.Configuraciones;
+using static Proyecto3erParcialGrupo3.Datas.Configuraciones;
 
 namespace Proyecto3erParcialGrupo3.Data
 {
-    public class Configuraciones
+    public class DbTaxisDbContext : DbContext
     {
-        // Configuraciones de Usuarios
-        public class UsuariosConfig : IEntityTypeConfiguration<Usuario>
-        {
-            public void Configure(EntityTypeBuilder<Usuario> builder)
-            {
-                // Elementos de Usuario
-                //
-                //--- Llave Primaria
-                builder.HasKey(x => x.UsuarioId);
-                //--- Relaciones
-                //   |
-                //   |--- Taxis
-                builder.HasMany(a => a.Taxis).WithOne(a => a.Usuario).HasForeignKey(a => a.UsuarioId);
-                //--- Placa
-                builder.Property(a => a.NombreUsuario).HasColumnType("varchar(20)").HasColumnName("Usuario");
-                //--- Observacion
-                builder.Property(a => a.Contraseña).HasColumnType("varchar(20)").HasColumnName("Contraseña");
-            }
-        }
+        // Constructor
+        public DbTaxisDbContext(DbContextOptions<DbTaxisDbContext> options) : base(options) { }
 
-        // Configuraciones de Taxis
-        public class TaxisConfig : IEntityTypeConfiguration<Taxi>
-        {
-            public void Configure(EntityTypeBuilder<Taxi> builder)
-            {
-                // Elementos de Taxi
-                //
-                //--- Llave Primaria
-                builder.HasKey(x => x.TaxiId);
-                //--- Relaciones
-                //   |
-                //   |--- Registros Diarios
-                builder.HasMany(a => a.RegistrosDiarios).WithOne(a => a.Taxi).HasForeignKey(a => a.TaxiId);
-                //--- Placa
-                builder.Property(a => a.Placa).HasColumnType("varchar(8)").HasColumnName("Placa");
-                //--- Observacion
-                builder.Property(a => a.Observacion).HasColumnType("varchar(255)").HasColumnName("Observacion");
-            }
-        }
+        // Entidades
+        //
+        //--- Usuarios
+        public DbSet<Usuario> Usuarios { get; set; }
+        //--- Taxis
+        public DbSet<Taxi> Taxi { get; set; }
+        //--- Departamentos
+        public DbSet<RegistroDiario> RegistroDiario { get; set; }
 
-        // Configuraciones de Registros Diarios
-        public class RegistrosDiariosConfig : IEntityTypeConfiguration<RegistroDiario>
+        // Configuraciones
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
-            public void Configure(EntityTypeBuilder<RegistroDiario> builder)
-            {
-                // Elementos de los Registros
-                //
-                //--- Llave Primaria
-                builder.HasKey(x => x.RegistroId);
-                //--- TotalDia
-                builder.Property(a => a.TotalDia).HasColumnType("decimal(8,2)").HasColumnName("TotalDia");
-                //--- PagoBase
-                builder.Property(a => a.PagoBase).HasColumnType("decimal(8,2)").HasColumnName("PagoBase");
-                //--- PagoConductor
-                builder.Property(a => a.PagoConductor).HasColumnType("decimal(8,2)").HasColumnName("PagoConductor");
-                //--- PagoDueño
-                builder.Property(a => a.PagoDueño).HasColumnType("decimal(8,2)").HasColumnName("PagoDueño");
-                //--- Gastos
-                builder.Property(a => a.Gastos).HasColumnType("decimal(8,2)").HasColumnName("Gastos");
-                //--- Observacion
-                builder.Property(a => a.Observacion).HasColumnType("varchar(255)").HasColumnName("Observacion");
-            }
+            // Usuarios
+            modelBuilder.ApplyConfiguration(new UsuariosConfig());
+            // Taxis
+            modelBuilder.ApplyConfiguration(new TaxisConfig());
+            // RegistrosDiarios
+            modelBuilder.ApplyConfiguration(new RegistrosDiariosConfig());
         }
     }
 }
