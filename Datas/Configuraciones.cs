23ae6624d1d5e155ce4d951dd506c47bb472e3a3
alejandrorodriguezujcv <alejandro.rodriguez@ujcv.edu.hn>
2023-11-19 20:50:04 -0600

    Config entidad cita

--- Datas/Configuraciones.cs
+++ Datas/Configuraciones.cs
@@ -1,6 +1,22 @@
-﻿namespace Proyecto3erParcialGrupo3.Datas
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Proyecto3erParcialGrupo3.Models;
+
+namespace Proyecto3erParcialGrupo3.Datas
 {
     public class Configuraciones
     {
+        public class CitasConfig : IEntityTypeConfiguration<Cita>
+        {
+            public void Configure(EntityTypeBuilder<Cita> builder)
+            {
+                //llave
+                builder.HasKey(x => x.IDCita);
+                //llave foranea
+                builder.HasOne(c => c.Usuario).WithMany(u => u.Citas).HasForeignKey(c => c.IDUsuario);
+
+                builder.Property(s => s.EstadoCita).HasColumnType("varchar(255)");
+            }
+        }
     }
 }
