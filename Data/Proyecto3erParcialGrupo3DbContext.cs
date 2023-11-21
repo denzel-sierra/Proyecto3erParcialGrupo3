056375cd575c5047c94f49af738d2ef1d20b1461
andreyardon <emersonardon1@gmail.com>
2023-11-20 20:13:31 -0600

    Actualizar dbcontext del proyecto

---
+++ Data/Proyecto3erParcialGrupo3DbContext.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Proyecto3erParcialGrupo3.Models;
+using static Proyecto3erParcialGrupo3.Data.Configuraciones;
+using static Proyecto3erParcialGrupo3.Datas.Configuraciones;
+
+namespace Proyecto3erParcialGrupo3.Data
+{
+    public class DbTaxisDbContext : DbContext
+    {
+        // Constructor
+        public DbTaxisDbContext(DbContextOptions<DbTaxisDbContext> options) : base(options) { }
+
+        // Entidades
+        //
+        //--- Usuarios
+        public DbSet<Usuario> Usuarios { get; set; }
+        //--- Cita
+        public DbSet<Cita> Cita { get; set; }
+        //--- CorrelativoSAR
+        public DbSet<CorrelativoSAR> CorrelativoSAR { get; set; }
+        //--- DetalleProductoFactura
+        public DbSet<DetalleProductoFactura> DetalleProductoFactura { get; set; }
+        //--- DetalleServicioFactura
+        public DbSet<DetalleServicioFactura> DetalleServicioFactura { get; set; }
+        //--- EncabezadoFactura
+        public DbSet<EncabezadoFactura> EncabezadoFactura { get; set; }
+        //--- Habitacion
+        public DbSet<Habitacion> Habitacion { get; set; }
+        //--- Producto
+        public DbSet<Producto> Producto { get; set; }
+        //--- Reserva
+        public DbSet<Reserva> Reserva { get; set; }
+        //--- ServicioHotel
+        public DbSet<ServicioHotel> ServicioHotel { get; set; }
+
+
+        // Configuraciones
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Usuarios
+            modelBuilder.ApplyConfiguration(new UsuarioConfig());
+            // Cita
+            modelBuilder.ApplyConfiguration(new CitasConfig());
+            // CorrelativoSAR
+            modelBuilder.ApplyConfiguration(new CorrelativoSARConfig());
+            // DetalleProductoFacturaConfig
+            modelBuilder.ApplyConfiguration(new DetalleProductoFacturaConfig());
+            // DetalleServicioFacturaConfig
+            modelBuilder.ApplyConfiguration(new DetalleServicioFacturaConfig());
+            // EncabezadoFacturaConfig
+            modelBuilder.ApplyConfiguration(new EncabezadoFacturaConfig());
+            // HabitacionConfig
+            modelBuilder.ApplyConfiguration(new HabitacionConfig());
+            // ProductoConfig
+            modelBuilder.ApplyConfiguration(new ProductoConfig());
+            // ReservaConfig
+            modelBuilder.ApplyConfiguration(new ReservaConfig());
+            // ServicioHotelConfig
+            modelBuilder.ApplyConfiguration(new ServicioHotelConfig());
+        }
+    }
+}
