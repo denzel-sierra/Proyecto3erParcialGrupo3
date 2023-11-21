a79de3364ed5ba3bdffeb745a58e596774d7bedb
denzel-sierra <denzel.sierra@ujcv.edu.hn>
2023-11-20 20:30:38 -0600

    Crear injeccón de dependencias para la base de datos

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using Proyecto3erParcialGrupo3.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<Proyecto3erParcialGrupo3DbContext> // Conexion a la Base de Datos
+    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
