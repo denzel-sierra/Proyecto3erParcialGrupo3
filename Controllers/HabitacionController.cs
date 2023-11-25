4c7678bcbe03179d462a96f31aa8f4d9abe7c204
alejandrorodriguezujcv <alejandro.rodriguez@ujcv.edu.hn>
2023-11-25 16:02:01 -0600

    Creacion Controlador y Index Habitacion

---
+++ Controllers/HabitacionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HotelManager.Controllers
+{
+    public class HabitacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
