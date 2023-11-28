356548b87f30a08076c170a0e8494ff4103daab4
alejandrorodriguezujcv <alejandro.rodriguez@ujcv.edu.hn>
2023-11-27 23:01:01 -0600

    Crud Habitaciones y otros cambios (Insertar)

---
+++ Models/VM/ReservasVM.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace HotelManager.Models.VM
+{
+    public class ReservasVM
+    {
+        public Guid IDReserva { get; set; }
+
+        
+        public Guid? IDUsuario { get; set; }
+
+       
+        public Guid IDHabitacion { get; set; }
+
+       
+        public Guid IDFactura { get; set; }
+
+        
+        public DateTime FechaCheckin { get; set; }
+
+       
+        public DateTime FechaCheckOut { get; set; }
+
+        public string EstadoReserva { get; set; }
+
+        // Relaciones
+        public HabitacionesVM Habitacion { get; set; }
+        //public EncabezadoFacturaVM EncabezadoFactura { get; set; }
+    }
+}
